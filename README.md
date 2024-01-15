1fe34ee09166cee58ed6f840d3c87a49871c5eee
David Lopes <155011706+DavidLopesTeixeira@users.noreply.github.com>
2024-01-15 11:31:48 -0300

    desenvolvimento de mixis, variáveis, classes para o scss. desenvolvimento do documento html e css. Movendo imagens e fontes para o projeto

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# ferramentas = Gulp, Sass ,
+# ferramentas = Gulp, Sass, gulp-imagemin
 # Gulp para fazer a compilação do Sass e a Minificação das imagens do projeto
 # Sass para simplificar e organizar o processo de desenvolvimento
+# Gulp-imagemin para fazer a compressão das imagens
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 //import sass, gulp, gulp-sass
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles() {
     // 1 - Recuperando todos os arquivos com a extençao .scss
@@ -11,9 +12,19 @@ function styles() {
     .pipe(gulp.dest('./dist/css'));
 };
 
+function images() {
+    // 1 - Recuperando todas as imagens que estiverem dentro da pasta
+    return gulp.src('./src/images/**/*')
+    // 2 -  executando a função imagemin
+    .pipe(imagemin())
+    // 3 - Enviando os arquivos prontos para o destino
+    .pipe(gulp.dest('./dist/images'));
+};
+
+
 
 // Exportando a função
-exports.default = styles;
+exports.default = gulp.parallel(styles, images);
 
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
