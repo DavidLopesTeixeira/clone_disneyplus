949d2ce18820a50068c1ab01b9945a409d6e39a4
David Lopes <155011706+DavidLopesTeixeira@users.noreply.github.com>
2024-01-17 13:31:13 -0300

    trabalhando na section faq 80% concluida

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
+
 document.addEventListener("DOMContentLoaded", function () {
-    const buttons = document.querySelectorAll("[data-tab-button]");
+    const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener("click", function (button) {
@@ -13,10 +15,21 @@ document.addEventListener("DOMContentLoaded", function () {
             button.target.classList.add("shows__tabs__button--is-active");
         });
     }
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener("click", openCloseAnswer)
+    }
 });
 
+function openCloseAnswer(element) {
+    const classe = 'faq__questions__item--is-open';
+    const parentElement = element.target.parentNode;
+
+    parentElement.classList.toggle(classe);
+}
+
 function removeActiveButton() {
-    const buttons = document.querySelectorAll("[data-tab-button]");
+    const buttons = document.querySelectorAll('[data-tab-button]');
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].classList.remove("shows__tabs__button--is-active");
@@ -30,3 +43,5 @@ function hideAllTabs() {
         tabsContainer[i].classList.remove("shows__list--is-active");
     }
 }
+
+
