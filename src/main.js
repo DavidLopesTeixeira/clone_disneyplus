a1c4a0f206a4c40dc457916b4edb1d3e43b1ae27
David Lopes <155011706+DavidLopesTeixeira@users.noreply.github.com>
2024-01-19 09:42:29 -0300

    Concluída a implementação do header. Durante esta etapa, foi dedicado esforço ao desenvolvimento do layout e à lógica subjacente que controla a visibilidade dinâmica dos links. Agora, os links são configurados para desaparecer de maneira suave, proporcionando uma experiência de usuário mais fluida e intuitiva.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,20 @@ document.addEventListener("DOMContentLoaded", function () {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');
 
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function () {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosDoHeader();
+        } else {
+            exibeElementosDoHeader();
+        }
+    })
+
+    // Seção de atrações, programação das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener("click", function (button) {
             const tabTarget = button.target.dataset.tabButton;
@@ -13,9 +27,10 @@ document.addEventListener("DOMContentLoaded", function () {
             tab.classList.add("shows__list--is-active");
             removeActiveButton();
             button.target.classList.add("shows__tabs__button--is-active");
-        });
-    }
+        })
+    };
 
+    // Seção FAQ, Accordion
     for (let i = 0; i < questions.length; i++) {
         questions[i].addEventListener("click", openCloseAnswer)
     }
@@ -27,6 +42,18 @@ function openCloseAnswer(element) {
 
     parentElement.classList.toggle(classe);
 };
+
+function ocultaElementosDoHeader() {
+    const header = document.querySelector('.header');
+    header.classList.add('header--is-hidden');
+};
+
+function exibeElementosDoHeader() {
+    const header = document.querySelector('.header');
+    header.classList.remove('header--is-hidden');
+};
+
+
 
 function removeActiveButton() {
     const buttons = document.querySelectorAll('[data-tab-button]');
